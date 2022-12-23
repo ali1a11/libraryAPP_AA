5e0f0d77b0eb3553deb1f22d14d11b5ac07fe82e
Ali Acilar <aliacilar@yahoo.com>
2022-12-23 08:59:25 +0100

    verification updates

--- src/test/java/com/cydeo/library/runner/CukesRunner.java
+++ src/test/java/com/cydeo/library/runner/CukesRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
                 "html:target/default-html-reports"},
         features = "src/test/resources/features",
         glue = "com/cydeo/library/step_definitions",
-        tags = "@WIPDB-API",// @LoginUsingFile    @students @librarians @login-api
+        tags = "@WIP_DB-API",
         dryRun = false
         )
 
